81f409fddfae1b33686eeffbf382060c418a98d6
Ole Mathias Heggem <olemathias.aa.heggem@gmail.com>
2021-08-11 22:14:51 +0200

    pycodestyle

--- fortiadc.py
+++ fortiadc.py
@@ -2,6 +2,7 @@ import requests
 from urllib3.util.retry import Retry
 from requests.adapters import HTTPAdapter
 
+
 class client:
     """Simple FortiADC Client"""
     token = None
@@ -9,19 +10,20 @@ class client:
 
     def __init__(self, **kwargs):
         for f in ["host", "password"]:
-            if f not in kwargs: raise ValueError("Missing required field: {0}".format(f))
+            if f not in kwargs:
+                raise ValueError("Missing required field: {0}".format(f))
 
         self.host = kwargs.get("host")
         self.base_url = "https://{0}".format(self.host)
         self.username = kwargs.get("username", "admin")
-        self.vdom = kwargs.get("vdom", "global") # TODO Use this
+        self.vdom = kwargs.get("vdom", "global")  # TODO Use this
 
         if self.token is None or self.last_access_time is None:
             self._login(self.username, kwargs.get("password"))
 
     def _login(self, username, password):
-        url = "{0}/api/user/login".format(self.base_url)
-        r = self._request(method="POST", path="user/login", json={"username":username, "password":password})
+        r = self._request(method="POST", path="user/login",
+                          json={"username": username, "password": password})
         if "token" not in r:
             raise Exception("Login failed")
 
@@ -30,7 +32,9 @@ class client:
 
     def _request(self, **kwargs):
         for f in ["path"]:
-            if f not in kwargs: raise ValueError("Missing required field: {0}".format(f))
+            if f not in kwargs:
+                raise ValueError("Missing required field: {0}".format(f))
+
         url = "{0}/api/{1}".format(self.base_url, kwargs.get("path"))
         headers = {
             "User-Agent": "fortadc-certbot-deploy-hook",
@@ -42,21 +46,42 @@ class client:
         # Retry 5 times, with backoff
         retries = Retry(total=5,
                         backoff_factor=0.5,
-                        status_forcelist=[ 500, 502, 503, 504 ])
+                        status_forcelist=[500, 502, 503, 504])
         s.mount('https://', HTTPAdapter(max_retries=retries))
 
         if kwargs.get("method", "GET").upper() == "GET":
-            r = s.get(url, params=kwargs.get("params", None), headers=headers)
-            if not r.ok: raise Exception("GET request to {0} failed! Got status_code {1}".format(url, r.status_code))
+            r = s.get(url, params=kwargs.get("params", None),
+                      headers=headers, timeout=5)
+            if not r.ok:
+                raise Exception("GET request to {0} failed!"
+                                " Got status_code {1}".format(
+                                    url, r.status_code))
         elif kwargs.get("method").upper() == "POST":
-            r = s.post(url, params=kwargs.get("params", None), json=kwargs.get("json", None), data=kwargs.get("data", None), files=kwargs.get("files", None), headers=headers)
-            if not r.ok: raise Exception("GET request to {0} failed! Got status_code {1}".format(url, r.status_code))
-        elif kwargs.get("method").upper() == "DELETE":
-            r = s.delete(url, params=kwargs.get("params", None), json=kwargs.get("json", None), data=kwargs.get("data", None), headers=headers)
-            if not r.ok: raise Exception("GET request to {0} failed! Got status_code {1}".format(url, r.status_code))
-        else:
-            raise ValueError("Unsupported method {}".format(kwargs.get("method")))
+            r = s.post(url, params=kwargs.get("params", None),
+                       json=kwargs.get("json", None),
+                       data=kwargs.get("data", None),
+                       files=kwargs.get("files", None),
+                       headers=headers, timeout=5)
 
+            if not r.ok:
+                raise Exception("POST request to {0} failed!"
+                                " Got status_code {1}".format(
+                                    url, r.status_code))
+        elif kwargs.get("method").upper() == "DELETE":
+            r = s.delete(url, params=kwargs.get("params", None),
+                         json=kwargs.get("json", None),
+                         data=kwargs.get("data", None),
+                         headers=headers, timeout=5)
+
+            if not r.ok:
+                raise Exception("DELETE request to {0} failed!"
+                                " Got status_code {1}".format(
+                                    url, r.status_code))
+        else:
+            raise ValueError("Unsupported method {}"
+                             .format(kwargs.get("method")))
+
+        # Set last_access_time on each request
         self.last_access_time = r.cookies["last_access_time"]
         return r.json()
 
@@ -66,7 +91,8 @@ class client:
 
     def upload_certificate_local(self, **kwargs):
         for f in ["mkey", "cert_path", "key_path"]:
-            if f not in kwargs: raise ValueError("Missing required field: {0}".format(f))
+            if f not in kwargs:
+                raise ValueError("Missing required field: {0}".format(f))
 
         data = {
             "mkey": kwargs.get("mkey"),
@@ -79,7 +105,8 @@ class client:
             "key": open(kwargs.get("key_path"), 'rb')
         }
 
-        r = self._request(method="POST", path="upload/certificate_local", data=data, files=files)
+        r = self._request(method="POST", path="upload/certificate_local",
+                          data=data, files=files)
         if "payload" not in r or r["payload"] != 0:
             print(r)
             raise Exception("Failed to upload cert")
@@ -88,7 +115,10 @@ class client:
         params = {
             "pkey": kwargs.get("pkey")
         }
-        r = self._request(path="system_certificate_local_cert_group_child_group_member", params=params)
+        r = self._request(
+            path="system_certificate_local_cert_group_child_group_member",
+            params=params
+        )
         return r["payload"]
 
     def add_certificate_local_group_member(self, **kwargs):
@@ -102,7 +132,12 @@ class client:
         params = {
             "pkey": kwargs.get("pkey")
         }
-        r = self._request(method="POST", path="system_certificate_local_cert_group_child_group_member", json=data, params=params)
+        r = self._request(
+            method="POST",
+            path="system_certificate_local_cert_group_child_group_member",
+            json=data, params=params
+        )
+
         # IDK why -154, but seems to work :)
         if "payload" not in r or (r["payload"] != 0 and r["payload"] != -154):
             print(r)
@@ -114,7 +149,12 @@ class client:
             "pkey": kwargs.get("pkey"),
             "mkey": kwargs.get("mkey"),
         }
-        r = self._request(method="DELETE", path="system_certificate_local_cert_group_child_group_member", params=params)
+        r = self._request(
+            method="DELETE",
+            path="system_certificate_local_cert_group_child_group_member",
+            params=params
+        )
+
         if "payload" not in r or r["payload"] != 0:
             print(r)
             raise Exception("Failed to delete cert from group")
--- main.py
+++ main.py
@@ -16,7 +16,9 @@ root.setLevel(logging.INFO)
 
 handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
 handler.setFormatter(formatter)
 root.addHandler(handler)
 
@@ -26,28 +28,35 @@ RENEWED_LINEAGE = os.environ.get("RENEWED_LINEAGE", None)
 # Not used if RENEWED_LINEAGE is set
 DOMAIN = os.environ.get("DOMAIN", None)
 
+if RENEWED_LINEAGE is not None and DOMAIN is not None:
+    logging.warning("DOMAIN and RENEWED_LINEAGE is set. "
+                    "Ignoring DOMAIN!")
+
 INTERMEDIATE = os.environ.get("INTERMEDIATE", "letsencrypt-rsa")
 LOCAL_CERT_GROUP = os.environ.get("LOCAL_CERT_GROUP", "certbot")
 
 USER = os.environ.get("FORTIADC_USER", "admin")
 HOST = os.environ.get("FORTIADC_HOST", None)
 if HOST is None:
-    logging.error("FORTIADC_HOST needs to be set. Example: FORTIADC_HOST=fortiadc.example.com")
-    raise ValueError("FORTIADC_HOST needs to be set. Example: FORTIADC_HOST=fortiadc.example.com")
+    raise ValueError("FORTIADC_HOST needs to be set!")
 PASSWORD = os.environ.get("FORTIADC_PASSWORD", None)
 if PASSWORD is None:
-    logging.error("FORTIADC_PASSWORD needs to be set. Example: FORTIADC_PASSWORD=mypassword")
-    raise ValueError("FORTIADC_PASSWORD needs to be set. Example: FORTIADC_PASSWORD=mypassword")
+    raise ValueError("FORTIADC_PASSWORD needs to be set.")
 if RENEWED_LINEAGE is not None:
     DOMAIN = str(os.path.basename(os.path.normpath(RENEWED_LINEAGE)))
 elif DOMAIN is None:
-    logging.error("RENEWED_LINEAGE or DOMAIN needs to be set")
     raise ValueError("RENEWED_LINEAGE or DOMAIN needs to be set")
 
-LETSENCRYPT_LIVE_FOLDER = os.environ.get("LETSENCRYPT_LIVE_FOLDER", "/etc/letsencrypt/live/")
+LETSENCRYPT_LIVE_FOLDER = os.environ.get(
+    "LETSENCRYPT_LIVE_FOLDER", "/etc/letsencrypt/live/"
+)
 
-CERT_PATH = os.environ.get("CERT_PATH", os.path.join(LETSENCRYPT_LIVE_FOLDER, DOMAIN, "fullchain.pem"))
-KEY_PATH = os.environ.get("KEY_PATH", os.path.join(LETSENCRYPT_LIVE_FOLDER, DOMAIN, "privkey.pem"))
+CERT_PATH = os.environ.get(
+    "CERT_PATH", os.path.join(LETSENCRYPT_LIVE_FOLDER, DOMAIN, "fullchain.pem")
+)
+KEY_PATH = os.environ.get(
+    "KEY_PATH", os.path.join(LETSENCRYPT_LIVE_FOLDER, DOMAIN, "privkey.pem")
+)
 
 if not os.path.exists(CERT_PATH):
     logging.error("Cert not found in \"{}\"".format(CERT_PATH))
@@ -56,39 +65,46 @@ if not os.path.exists(KEY_PATH):
     logging.error("Key not found in \"{}\"".format(KEY_PATH))
     raise Exception("Key not found in \"{}\"".format(KEY_PATH))
 
-logging.info("Domain: \"{}\". Will upload to \"{}\" and add to group \"{}\".".format(DOMAIN, HOST, LOCAL_CERT_GROUP))
+logging.info(
+    "Domain: \"{}\". Will upload to \"{}\""
+    "and add to group \"{}\".".format(DOMAIN, HOST, LOCAL_CERT_GROUP)
+)
 logging.info("CERT_PATH {}".format(CERT_PATH))
 logging.info("KEY_PATH {}".format(KEY_PATH))
 
-cert_name = DOMAIN.replace(".","_")
+cert_name = DOMAIN.replace(".", "_")
 
 client = fortiadc.client(host=HOST, username=USER, password=PASSWORD)
 logging.info("Connected to FortiADC: {}.".format(HOST))
-
-#p = re.compile('^certbot_(.+)_(\d{8})-(\w+)$')
-#certs = [cert for cert in client.get_certificate_local() if p.match(cert["mkey"]) and p.match(cert["mkey"]).group(1) == cert_name]
 
 # To not have duplicates
 serial = ''.join(random.choices(string.ascii_uppercase, k=3))
 current_date = date.today().strftime("%Y%m%d")
 mkey = "certbot_{0}_{1}-{2}".format(cert_name, current_date, serial)
 
-client.upload_certificate_local(mkey=mkey, cert_path=CERT_PATH, key_path=KEY_PATH)
-client.add_certificate_local_group_member(pkey=LOCAL_CERT_GROUP, mkey=mkey, intermediate=INTERMEDIATE)
+client.upload_certificate_local(
+    mkey=mkey, cert_path=CERT_PATH, key_path=KEY_PATH
+)
+client.add_certificate_local_group_member(
+    pkey=LOCAL_CERT_GROUP, mkey=mkey, intermediate=INTERMEDIATE
+)
 
-logging.info("\"{}\" uploaded and added to group! Sleep 5 next".format(mkey))
+logging.info("\"{}\" uploaded and added to group!".format(mkey))
 
 # To make sure the new cert is ready before removing the old
+logging.info("Sleep 5 to make sure the new cert is ready!")
 time.sleep(5)
 
 # Get the certficate(s) from FortiADC if exists
-p = re.compile('^certbot_(.+)_(\d{8})-(\w+)$')
-certs = [cert for cert in client.get_certificate_local_group_members(pkey=LOCAL_CERT_GROUP) if p.match(cert["local_cert"]) and p.match(cert["local_cert"]).group(1) == cert_name]
+p = re.compile(r'^certbot_(.+)_(\d{8})-(\w+)$')
 
-for c in certs:
-    if not c["local_cert"] == mkey:
-        client.delete_certificate_local_group_member(pkey=LOCAL_CERT_GROUP, mkey=c["mkey"])
-        logging.info("Removed \"{}\" from group \"{}\"".format(c["local_cert"], LOCAL_CERT_GROUP))
+for cert in client.get_certificate_local_group_members(pkey=LOCAL_CERT_GROUP):
+    if (p.match(cert["local_cert"]) and
+            p.match(cert["local_cert"]).group(1) == cert_name and
+            cert["local_cert"] != mkey):
 
-# ^certbot_(.+)_(\d{8})-(\d+)$
-# ^certbot_gathering_systems_(\d{8})-(\d+)$
+        client.delete_certificate_local_group_member(
+            pkey=LOCAL_CERT_GROUP, mkey=cert["mkey"]
+        )
+        logging.info("Removed \"{}\" from group"
+                     " \"{}\"".format(cert["local_cert"], LOCAL_CERT_GROUP))
